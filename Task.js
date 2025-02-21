bd1707317073a72581ef378101e5b95c80ebb08c
bairagidevendra30@gmail.com <bairagidevendra30@gmail.com>
2025-02-21 12:55:41 +0530

    This is javascripttask

---
+++ Task.js
@@ -0,0 +1,45 @@
+/////////////reverce string//////////
+
+// let arr = ["dev","pradeep","piyush","kamlesh"];
+
+//     let h = arr.reverse();
+//     console.log(h);
+    
+/////////Filter odd  or even //////
+
+// let a = [1,2,3,4,5,6,7,8,9,20]
+// var a1 =[] ;
+// var a2 =[] ;
+// for(var i =0;i<a.length;i++){
+//     if(a[i]%2==0){
+//         a1.push(a[i])
+//     }
+//     else{
+//         a2.push(a[i])
+//     }
+// }
+// console.log(a1)
+// console.log(a2)
+
+
+
+///////////factorial/////////
+
+// var n = 0;
+// var sum = 1
+// for(var i = 1;i<=n;i++){
+//     if(n==0){
+//         console.log(sum);
+//         break ;
+//     }
+//     else
+//  sum= sum * i
+// }
+// console.log(sum);
+
+
+
+        
+    
+
+
